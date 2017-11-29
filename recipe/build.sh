59b3409cf7729cd3c34f6280a6f718dfba2c0ad1
Théo Segonds <theo.segonds@inria.fr>
2017-11-29 17:42:28 +0000

    Add arm HW flagswq

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,9 @@
 
 autoreconf -fiv
 
-# Set here any HW specific flags (usually at CFLAGS)
+ #Set here any HW specific flags (usually at CFLAGS)
+export CFLAGS="-mcpu=cortex-a7 -mfpu=neon-vfpv4 -ftree-vectorize -mfloat-abi=hard -fPIC -O3"
+export CXXFLAGS="-mcpu=cortex-a7 -mfpu=neon-vfpv4 -ftree-vectorize -mfloat-abi=hard -fPIC -O3"
 ./configure --prefix=$PREFIX \
     --enable-shared=yes \
     --enable-static=yes \
